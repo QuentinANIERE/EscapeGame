b9d068030ab16baa48539efc09c53c3ac4b5d3e4
Quentin ANIERE <quentin.aniere@gmail.com>
2020-05-16 02:43:55 +0200

    Optimisation du code
    Optimisation des fonctions de chiffrement : Au lieu de d'abord convertir une lettre en entiers, on récupere directement sa position dans l'alphabet avec une chaine alphabet

--- Chiffrement/JS/script.js
+++ Chiffrement/JS/script.js
@@ -1,315 +1,50 @@
-const transformationChiffres = (chaine) => {
+function chiffrement(chaine, clef) {
 
-    /* Cette fonction transforme une chaine en liste d'entiers selon la position de chaque caractere dans l'alphabet.
-    Exemple => a = 1, f = 6, z = 26 */  
-    
-    let listeCarateres = chaine.split('');
-    //On splite chaque caractere de la chaine en une liste ("Hello" => ["H", "e", "l", "l"; "o"])
-    let nouvelleListe = [];
-    
-    for (caratere of listeCarateres) {
+    let resultat = "";
 
-        switch(caratere) {
+    for (lettre of chaine) {
+    //On récupere chaque lettre de la chaine 
+        for (index = 0; index < alphabet.length; index++) {
+        //On récupere chaque indice d'index de l'alphabet
 
-            case " ":
-                nouvelleListe.push(0)
-                break; //Le break est indispensable car sinon cela execute le bloc d'en dessous même si la condition est false
+            if (lettre == alphabet[index]) {
+            //Si une lettre de la chaine correspondant à une lettre de l'alphabet
+            //Exemple = si a = a, on récupere la position 1 à laquelle on ajoutera la clef de chiffrement
+                nouveauRang = index + clef;
 
-            case "a":
-                nouvelleListe.push(1);
-                break;
-
-            case "b":
-                nouvelleListe.push(2);
-                break;
-            
-            case "c":
-                nouvelleListe.push(3);
-                break;
-
-            case "d":
-                nouvelleListe.push(4);
-                break;
-
-            case "e":
-                nouvelleListe.push(5);
-                break;
-
-            case "f":
-                nouvelleListe.push(6);
-                break;
-            
-            case "g":
-                nouvelleListe.push(7);
-                break;
-
-            case "h":
-                nouvelleListe.push(8);
-                break;
-
-            case "i":
-                nouvelleListe.push(9);
-                break;
-
-            case "j":
-                nouvelleListe.push(10);
-                break;
-            
-            case "k":
-                nouvelleListe.push(11);
-                break;
-            
-            case "l":
-                nouvelleListe.push(12);
-                break;
-
-            case "m":
-                nouvelleListe.push(13);
-                break;
-
-            case "n":
-                nouvelleListe.push(14);
-                break;
-
-            case "o":
-                nouvelleListe.push(15);
-                break;
-
-            case "p":
-                nouvelleListe.push(16);
-                break;
-
-            case "q":
-                nouvelleListe.push(17);
-                break;
-
-            case "r":
-                nouvelleListe.push(18);
-                break;
-
-            case "s":
-                nouvelleListe.push(19);
-                break;
-
-            case "t":
-                nouvelleListe.push(20);
-                break;
-
-            case "u":
-                nouvelleListe.push(21);
-                break;
-
-
-            case "v":
-                nouvelleListe.push(22);
-                break;
-
-            case "w":
-                nouvelleListe.push(23);
-                break;
-
-            case "x":
-                nouvelleListe.push(24);
-                break;
-
-            case "y":
-                nouvelleListe.push(25);
-                break;
-            
-            case "z":
-                nouvelleListe.push(26);
-                break;
-
-            default:
-                nouvelleListe.push("?");
-                break;
-
+                while (nouveauRang > 26) {
+                //Si on dépasse la lettre z, on revient à l'espace
+                    nouveauRang -= 26;
+                }
+                resultat += alphabet[nouveauRang];
+            }
         }
-    }
-
-    return nouvelleListe;
-}
-
-const transformationLettres = (liste) => {
-
-    /* Cette fonction transforme les entiers en lettres, selon leur position dans l'alphabet latin.
-    exemple => 1 = a, 6 = f, 26 = z */  
-    
-    let nouvelleListe = [];
-    
-    for (entier of liste) {
-    //On effectue l'opération autant de fois qu'il y'a d'index dans la liste. 
-    //Les entiers sont asignés tour à tour a la variable caractere
-
-        switch(entier) {
-
-            case 0:
-                nouvelleListe.push(" ")
-                break; //Le break est indispensable car sinon cela execute le bloc d'en dessous même si la condition est false
-
-            case 1:
-                nouvelleListe.push("a");
-                break;
-
-            case 2:
-                nouvelleListe.push("b");
-                break;
-            
-            case 3:
-                nouvelleListe.push("c");
-                break;
-
-            case 4:
-                nouvelleListe.push("d");
-                break;
-
-            case 5:
-                nouvelleListe.push("e");
-                break;
-
-            case 6:
-                nouvelleListe.push("f");
-                break;
-            
-            case 7:
-                nouvelleListe.push("g");
-                break;
-
-            case 8:
-                nouvelleListe.push("h");
-                break;
-
-            case 9:
-                nouvelleListe.push("i");
-                break;
-
-            case 10:
-                nouvelleListe.push("j");
-                break;
-            
-            case 11:
-                nouvelleListe.push("k");
-                break;
-            
-            case 12:
-                nouvelleListe.push("l");
-                break;
-
-            case 13:
-                nouvelleListe.push("m");
-                break;
-
-            case 14:
-                nouvelleListe.push("n");
-                break;
-
-            case 15:
-                nouvelleListe.push("o");
-                break;
-
-            case 16:
-                nouvelleListe.push("p");
-                break;
-
-            case 17:
-                nouvelleListe.push("q");
-                break;
-
-            case 18:
-                nouvelleListe.push("r");
-                break;
-
-            case 19:
-                nouvelleListe.push("s");
-                break;
-
-            case 20:
-                nouvelleListe.push("t");
-                break;
-
-            case 21:
-                nouvelleListe.push("u");
-                break;
-
-
-            case 22:
-                nouvelleListe.push("v");
-                break;
-
-            case 23:
-                nouvelleListe.push("w");
-                break;
-
-            case 24:
-                nouvelleListe.push("x");
-                break;
-
-            case 25:
-                nouvelleListe.push("y");
-                break;
-            
-            case 26:
-                nouvelleListe.push("z");
-                break;
-
-            default:
-                nouvelleListe.push("?");
-                break;
-
-        }
-    }
-
-    let chaine = "";
-
-    for (caratere of nouvelleListe) {
-    //On ajout chaque caractere traduit a une chaine
-        
-        chaine += caratere;
-    }
-
-    return chaine;
-}
-
-
-const chiffrement = (liste, clef) => {
-    
-    let resultat = [];
-
-    for (chiffre of liste) {     
-    //On récupére chaque chiffre de la liste tour à tour
-
-        chiffre += clef;
-        //On ajoute la clé de chiffrement
-
-        while (chiffre > 26) {
-        //Si le résultat obtenu est plus grand que 26, "la roue à fait à tour" donc on enleve 26. 
-        //On repete l'opération autant que nécéssaire
-            chiffre -= 26;
-        }
-
-        resultat.push(chiffre);
     }
 
     return resultat;
 }
 
-const dechiffrement = (liste, clef) => {
-    
-    let resultat = [];
+function déchiffrement(chaine, clef) {
 
-    for (chiffre of liste) {     
-    //On récupére chaque chiffre de la liste tour à tour
+    let resultat = "";
 
-        chiffre -= clef;
-        //On soustrait la clé de chiffrement
+    for (lettre of chaine) {
+    //On récupere chaque lettre de la chaine 
+        for (index = 0; index < alphabet.length; index++) {
+        //On récupere chaque indice d'index de l'alphabet
 
-        while (chiffre < 0) {
-        //Si le résultat obtenu est plus petit que 0, "la roue à fait à tour" donc on ajoute 26. 
-        //On repete l'opération autant que nécéssaire
-            chiffre += 26;
+            if (lettre == alphabet[index]) {
+            //Si une lettre de la chaine correspondant à une lettre de l'alphabet
+            //Exemple = si a = a, on récupere la position 1 à laquelle on ajoutera la clef de chiffrement
+                nouveauRang = index - clef;
+
+                while (nouveauRang < 0) {
+                //Si on dépasse la lettre a, on revient à z
+                    nouveauRang += 26;
+                }
+                resultat += alphabet[nouveauRang];
+            }
         }
-
-        resultat.push(chiffre);
     }
 
     return resultat;
@@ -319,18 +54,21 @@ function EntierAléatoire(ChiffreMax) {
     return Math.floor(Math.random() * Math.floor(ChiffreMax));
   }
 
-let PositionsGeneral = ["au bar", "a la petanque"]
-let indexRandom = Math.random();
+const alphabet = " abcdefghijklmnopqrstuvwxyz";
+//Définition de l'alphabet + l'espace à l'index 0
 
-indexRandom *= PositionsGeneral.length;
-indexRandom = Math.floor(indexRandom);
+let PositionsGeneral = ["au bar", "a la petanque"];
+let indexRandom = EntierAléatoire(PositionsGeneral.length);
+//On choisi une position aléatoire pour le général parmis la liste de positions possibles
 
 let clef = EntierAléatoire(9999);
+//On choisi un clef de chiffrement aléatoire à quatre chiffre 
 
-let message = transformationChiffres(PositionsGeneral[indexRandom]);
+let MessageChiffre = chiffrement(PositionsGeneral[indexRandom], clef);
+//On chiffre la position
 
-message = chiffrement(message, clef);
-message = transformationLettres(message);
+console.log(MessageChiffre);
 
-let AffichageMessage = document.getElementById("MessageChiffre");
-AffichageMessage.innerHTML = message;
+let banderole = document.getElementById("MessageChiffre");
+banderole.innerHTML = MessageChiffre;
+// On récupere la banderole HTML et on affiche le message chiffrée
