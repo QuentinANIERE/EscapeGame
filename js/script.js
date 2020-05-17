4af3cb25207a3b533722880735aa9e26dead3cce
Quentin ANIERE <quentin.aniere@gmail.com>
2020-05-17 23:15:56 +0200

    Sélectionneur de clef fonctionel

--- js/script.js
+++ js/script.js
@@ -69,6 +69,38 @@ function estMultiplede26(Chiffre) {
     }
 }
 
+function changementsValeur(opération){
+
+    opération = opération.split("_");
+    //On récupere l'opération à effectuer (plus ou moins) et la case à changer
+
+    caseAff = parseInt(opération[1]) - 1;
+    //On converti l'id en index de la liste, la case avec l'id 1 étant stocké à l'indice 0
+
+    if (opération[0] == "+") {
+
+        ancienneValeur = listeCase[caseAff].innerHTML;
+        nouvelleValeur = parseInt(ancienneValeur) + 1;
+
+        if (nouvelleValeur == 10) {
+            nouvelleValeur = 0;
+        }
+
+        listeCase[caseAff].innerHTML = nouvelleValeur;
+
+    } else {
+
+        ancienneValeur = listeCase[caseAff].innerHTML;
+        nouvelleValeur = parseInt(ancienneValeur) - 1;
+
+        if (nouvelleValeur == -1) {
+            nouvelleValeur = 9;
+        }
+
+        listeCase[caseAff].innerHTML = nouvelleValeur;
+    }
+}
+
 
 
 const alphabet = " abcdefghijklmnopqrstuvwxyz";
@@ -91,11 +123,45 @@ while (estMultiplede26(clef)) {
 console.log(clef); //Temporaire pour le dev
 
 let MessageChiffre = chiffrement(PositionsGeneral[indexRandom], clef);
-//On chiffre la position
 
 let banderole = document.getElementById("MessageChiffre");
 banderole.innerHTML = MessageChiffre;
 // On récupere la banderole HTML et on affiche le message chiffré
 
+let case1 = document.getElementById("case1");
+let case2 = document.getElementById("case2");
+let case3 = document.getElementById("case3");
+let case4 = document.getElementById("case4");
+//Récupération des cases d'affichage
+
+let listeCase = [case1, case2, case3, case4];
+
+for(caseAff of listeCase) {
+
+    caseAff.innerHTML = EntierAléatoire(10);
+}
+
 document.oncontextmenu = new Function("return false");
 //Fonction qui empêche d'afficher le menu du clic droit
+
+let h_1 = document.getElementById("+_1");
+let h_2 = document.getElementById("+_2");
+let h_3 = document.getElementById("+_3");
+let h_4 = document.getElementById("+_4");
+
+let b_1 = document.getElementById("-_1");
+let b_2 = document.getElementById("-_2");
+let b_3 = document.getElementById("-_3");
+let b_4 = document.getElementById("-_4");
+//Récuperation des bouttons HTML ainsi que des cases
+
+h_1.addEventListener("click", function() { changementsValeur(this.id); } );
+h_2.addEventListener("click", function() { changementsValeur(this.id); } );
+h_3.addEventListener("click", function() { changementsValeur(this.id); } );
+h_4.addEventListener("click", function() { changementsValeur(this.id); } );
+
+b_1.addEventListener("click", function() { changementsValeur(this.id); } );
+b_2.addEventListener("click", function() { changementsValeur(this.id); } );
+b_3.addEventListener("click", function() { changementsValeur(this.id); } );
+b_4.addEventListener("click", function() { changementsValeur(this.id); } );
+//Au clic, on lance la fonction avec comme argument l'id HTML du boutton cliqué, ou de la case
