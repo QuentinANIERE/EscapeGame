ac4fc4a5a34500d20934079d1fa2d244fee73c9d
Quentin ANIERE <quentin.aniere@gmail.com>
2020-05-22 21:36:07 +0200

    Ajout de la première question

--- js/aide.js
+++ js/aide.js
@@ -1,1 +1,50 @@
-console.log("Script chargé")
+function demandeAide() {
+
+    switch(saisie.value) {
+        
+        default:
+
+            texte.innerHTML = "Je n'ai pas compris...";
+            break;
+
+        case "":
+        case " ":
+        case "  ":
+        case "    ":
+
+            break;
+    
+        case "C'est quoi ?":
+        case "C'est quoi":
+        case "c'est quoi ?":
+        case "C est quoi ?":
+        case "c koi":
+
+            texte.innerHTML = "C'est un message chiffré ennemi !";
+            break;
+
+
+
+    }
+
+    saisie.value = "";
+}
+
+function detection() {
+    
+    if (event.key == "Enter") {
+    //Si l'utilisateur a appuyé sur la touche entrée
+        demandeAide();
+    }
+}
+
+let texte = document.getElementById("texteGunther");
+let saisie = document.getElementById("saisie");
+let boutton = document.getElementById("boutton");
+//Récupération des élements HTML
+
+boutton.addEventListener("click", demandeAide);
+//Ajout d'un callback au clic sur le boutton
+
+document.addEventListener("keypress", detection);
+//Ajout d'un callback pour chaque pressée, la fonction vérifie si c'est la touche entrée
